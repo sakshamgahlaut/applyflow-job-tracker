c1c89ba29dcb00eb9a1e5301ca77bb20fafd9270
Saksham Gahlaut <sakshamgahlaut111@gmail.com>
2025-12-21 14:39:38 +0530

    backend connection

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config() ;
+
+const app = express() ;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get("/", (req, res)=>{
+    res.send("ApplyFlow API is running");
+})
+
+
+const PORT = process.env.PORT || 5000 ;
+app.listen(PORT, ()=> {
+    console.log(`Server is listening on port ${PORT}`);
+});
